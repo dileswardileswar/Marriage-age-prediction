61f9712377ce69e72f9ea0840d65eaab6779ea9a
Dileswar Sahu <dileswar.sahu999@gmail.com>
2020-12-07 02:08:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import flask
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify, render_template
+
+
+app = flask.Flask(__name__)
+model = pickle.load(open(r'F:\8.End_to_End_Project\2.Marriage Age Prediction\My COde\marriage_age_predict_model.pkl','rb'))
+app.config["DEBUG"] = True
+
+
+# main index page route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():    
+    predicted_age_of_marriage = [int(value) for value in request.form.values()]
+    final_predicted_age_of_marriage = [np.array(predicted_age_of_marriage)]
+    prediction = model.predict(final_predicted_age_of_marriage)
+    output = int(prediction[0])
+    return render_template('index.html', prediction_text = "Your Predicted Year of Marriage : {}".format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
