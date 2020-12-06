e5f14355333db585b817d8df8b210c5bfb0e52f7
Dileswar Sahu <dileswar.sahu999@gmail.com>
2020-12-06 21:28:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict',methods=['GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
     from sklearn.externals import joblib
     model = joblib.load('marriage_age_predict_model.ml')
